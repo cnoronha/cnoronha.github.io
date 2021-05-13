6f6fb8ccc823faf1bc30f9beea46386a7057ca01
colin <colin@example.com>
2021-05-12 21:15:41 -0700

    More dropdown project work

---
+++ js/content.dropdown.js
@@ -0,0 +1,8 @@
+function contentdropdown() {
+    var x = document.getElementById('solarboat');
+    if (x.style.display == 'none') {
+        x.style.transition = "all 0.5s ease-in-out";
+    } else {
+        x.style.transition = "all 0.5s ease-in-out";
+    }
+}
